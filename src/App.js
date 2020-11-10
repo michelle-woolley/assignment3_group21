06ae1b88891269338fa81f1d9e9be83035b682ca
Lachlan Cresswell <lachy93@gmail.com>
2020-11-10 21:07:30 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from './Components/Search';
+import './App.css';
+
+function App() {
+  return (
+	  <div id="app-container">
+		  <Search/>
+	  </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Search.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import './Search.css';
+import db from '../database.js';
+
+class Search extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			query: '',
+			results: db,
+			loading: false,
+			message: '',
+			totalResults: db.length,
+			totalPages: 1,
+			currentPageNo: 1,
+		};
+
+		this.cancel = '';
+	}
+
+
+	/**
+	 * Get the Total Pages count.
+	 *
+	 * @param total
+	 * @param denominator Count of results per page
+	 * @return {number}
+	 */
+	getPageCount = (total, denominator) => {
+		const divisible = 0 === total % denominator;
+		const valueToBeAdded = divisible ? 0 : 1;
+		return Math.floor(total / denominator) + valueToBeAdded;
+	};
+
+	/**
+	 * Fetch the search results and update the state with the result.
+	 * Also cancels the previous query before making the new one.
+	 *
+	 * @param {int} updatedPageNo Updated Page No.
+	 * @param {String} query Search Query.
+	 *
+	 */
+	fetchSearchResults = (updatedPageNo = '', query) => {
+
+		if (this.cancel) {
+			this.cancel.cancel();
+		}
+
+		const res = db.filter(e => e.title.toLowerCase().includes(query.toLowerCase()) || e.tags.filter(t => t.toLowerCase().includes(query.toLowerCase())).length);
+		
+		const total = res.length;
+		const totalPagesCount = this.getPageCount(total, 20);
+		const resultNotFoundMsg = !res.length ? 'There are no more search results. Please try a new search' : '';
+		this.setState({
+			results: res,
+			message: resultNotFoundMsg,
+			totalResults: total,
+			totalPages: totalPagesCount,
+			currentPageNo: updatedPageNo,
+			loading: false
+		});
+	};
+
+	handleOnInputChange = (event) => {
+		const query = event.target.value;
+		this.setState({ query, loading: true, message: '' }, () => {
+			this.fetchSearchResults(1, query);
+		});
+	};
+
+	/**
+	 * Fetch results according to the prev or next page requests.
+	 *
+	 * @param {String} type 'prev' or 'next'
+	 */
+	handlePageClick = (type, event) => {
+		event.preventDefault();
+		const updatePageNo = 'prev' === type
+			? this.state.currentPageNo - 1
+			: this.state.currentPageNo + 1;
+
+		if (!this.state.loading) {
+			this.setState({ loading: true, message: '' }, () => {
+				this.fetchSearchResults(updatePageNo, this.state.query);
+			});
+		}
+	};
+
+	renderSearchResults = () => {
+		const { results } = this.state;
+		if (results.length) {
+			return (
+				<div className="results">
+					{results.map((result, i) => {
+						return (
+							<div class="result-item">
+										<img className="image"
+											src={`${result.img}`}
+											alt="Ocimum basilicum" />
+								<div class="result-info">
+									<h4 class="image-username">{`${result.title}`}</h4>
+									<div class="result-tag-div">
+										{result.tags.map(t => <span class="result-tag">{`${t}`}</span>)}
+									</div>
+									<span class="result-desc">{`${result.desc}`}</span>
+
+								</div>
+							</div>
+						)
+					})}
+				</div>
+			)
+		}
+	};
+
+	render() {
+		const { query, message } = this.state;
+
+		return (
+			<div className="container">
+				{/*	Heading*/}
+				<h2 className="heading">Toxin Database</h2>
+				{/* Search Input*/}
+				<label className="search-label" htmlFor="search-input">
+					<input
+						type="text"
+						name="query"
+						value={query}
+						id="search-input"
+						placeholder="Search..."
+						onChange={this.handleOnInputChange}
+					/>
+					<i className="fa fa-search search-icon" aria-hidden="true" />
+				</label>
+
+				{/*	Error Message*/}
+				{message && <p className="message">{message}</p>}
+
+				{/*	Result*/}
+				{this.renderSearchResults()}
+
+			</div>
+		)
+	}
+}
+
+export default Search
