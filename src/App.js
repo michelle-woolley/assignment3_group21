4d85fcff1bab9bb945021e3401cdc27fa369cca7
Lachlan Cresswell <lachy93@gmail.com>
2020-11-11 20:00:28 +1100

    Material-UI additions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React from 'react'
 import Search from './Components/Search';
 import './App.css';
+import Typography from '@material-ui/core/Typography'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function App() {
-  return (
-	  <div id="app-container">
-		  <Search/>
-	  </div>
-  );
+export default function App() {
+
+	return (
+		<div>
+			<AppBar position="static">
+				<Toolbar variant="dense">
+					<IconButton edge="start">
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" color="inherit">
+						Toxins
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<div id="app-container">
+				<Search />
+			</div>
+		</div>
+	);
 }
 
-export default App;
